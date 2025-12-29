fd22815d6b72c148a125f485d1c35c5d16ca1fd7
Jaimel126 <dugbehcjaimel@gmail.com>
2025-12-29 00:40:41 +0000

    Create firebase.js

---
+++ firebase.js
@@ -0,0 +1,13 @@
+<script src="https://www.gstatic.com/firebasejs/9.23.0/firebase-app-compat.js"></script>
+<script src="https://www.gstatic.com/firebasejs/9.23.0/firebase-auth-compat.js"></script>
+
+<script>
+  const firebaseConfig = {
+    apiKey: "PASTE_YOURS_HERE",
+    authDomain: "PASTE_YOURS_HERE",
+    projectId: "PASTE_YOURS_HERE",
+    storageBucket: "PASTE_YOURS_HERE",
+    messagingSenderId: "PASTE_YOURS_HERE",
+    appId: "PASTE_YOURS_HERE"
+  };
+const app = initializeApp(firebaseConfig);
