b24eef1492779af52fac43fb4a35fafc0f4962d1
Jaimel126 <dugbehcjaimel@gmail.com>
2025-12-29 01:41:19 +0000

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,8 +1,7 @@
-<script src="https://www.gstatic.com/firebasejs/9.23.0/firebase-app-compat.js"></script>
-<script src="https://www.gstatic.com/firebasejs/9.23.0/firebase-auth-compat.js"></script>
-
-<script>
-  const firebaseConfig = {
+// Firebase SDKs
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-auth.js";
+const firebaseConfig = {
   apiKey: "AIzaSyDidOESF8lPLvFs5ryNcJW0VrP0CnvRDHQ",
   authDomain: "eduverse-bf551.firebaseapp.com",
   projectId: "eduverse-bf551",
@@ -10,5 +9,8 @@
   messagingSenderId: "1048542150292",
   appId: "1:1048542150292:web:50a011a1919f75cc6c3e81"
 };
-
-
+}// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+// Make auth usable globally
+window.auth = auth;
