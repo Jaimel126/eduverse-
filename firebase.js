b8772f8d1baaa59feac193a1b46dc186eb5824bb
Jaimel126 <dugbehcjaimel@gmail.com>
2025-12-29 01:16:42 +0000

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -3,11 +3,13 @@
 
 <script>
   const firebaseConfig = {
-    apiKey: "PASTE_YOURS_HERE",
-    authDomain: "PASTE_YOURS_HERE",
-    projectId: "PASTE_YOURS_HERE",
-    storageBucket: "PASTE_YOURS_HERE",
-    messagingSenderId: "PASTE_YOURS_HERE",
-    appId: "PASTE_YOURS_HERE"
-  };
+  apiKey: "AIzaSyDidOESF8lPLvFs5ryNcJW0VrP0CnvRDHQ",
+  authDomain: "eduverse-bf551.firebaseapp.com",
+  projectId: "eduverse-bf551",
+  storageBucket: "eduverse-bf551.firebasestorage.app",
+  messagingSenderId: "1048542150292",
+  appId: "1:1048542150292:web:50a011a1919f75cc6c3e81"
+};
+
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
